3ff032a24761a699a5a43a38af720a375d9ffe98
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-05-12 16:51:22 +0900

    🧂 test: add snapshot test with jest

--- test/hello-cdk.test.ts
+++ test/hello-cdk.test.ts
@@ -1,13 +1,9 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {SynthUtils} from "@aws-cdk/assert";
 import * as cdk from '@aws-cdk/core';
 import * as HelloCdk from '../lib/hello-cdk-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Lambda Stack', () => {
+  const app = new cdk.App();
+  const stack = new HelloCdk.HelloCdkStack(app, 'MyTestStack');
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
